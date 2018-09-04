8df97b8c916a0891ccb79465d04c4a2279141789
Zaytsev_mikhail <zaytcev_mikhail@mail.ru>
2018-09-04 19:39:27 +0400

    created "Point" class

---
+++ Snake-csharp/Snake-csharp/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_csharp
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+       
+        
+        public void Draw()
+        {
+
+            Console.SetCursorPosition(p.x, p.y);
+            Console.Write(p.sym);
+        }
+    }
+}
